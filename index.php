f34718329e31fa1175af09de8cdf6bd8c338df39
jamari266 <47312075+jamari266@users.noreply.github.com>
2022-03-17 19:05:30 -0700

    3/17/2022

--- index.php
+++ index.php
@@ -1,27 +1,145 @@
+<!DOCTYPE html>
+<html lang= "en">
+    <head>
+        <meta charset= "UTF-8" >
+        <meta name= "viewport" content= "width=device-width" initial-scale= "1.0" >
+        <meta http-equiv= "X-UA-Compatible"content = "ie-edge" >
+        <title> Home Page </title>
+        <link href= "css/test.styles.css" type="text/css" rel= "stylesheet">
+    </head>
 
-<?php
-include ('config.php');
-include ('includes/header.php');
-?>
+    <body>
+        <header>
+            <div id="inner-header">
+                <h1> My Portal Page!</h1>
+                <img class="img-1MAMP"src="images/1MAMP-Logo.png" alt="my 1MAMP">
+
+                
+
+            </div> 
+            <!--end inner header-->
+
+        </header>
     
-        <div id= "wrapper" >
-        <div id= "hero" > 
+        <div id="wrapper">
+        <div id="hero"> 
+            <nav>
+                
+                <ul>
+                    
+                    <li><a href="website/daily.php">Switch</a></li>
+                    <li><a href="troubleshoot.php">Troubleshoot</a></li>
+                    <li><a href="website/contact.php">Email</a></li>
+                    <li><a href="calculator.php">Calculator</a></li>
+                    <li><a href="website/gallery.php">Gallery</a></li>
+                    <li><a href="database.php">Database</a></li>
+                </ul>
+            </nav>
+        </div> <!--end hero-->
 
-        </div> <!--end hero--> 
-
-        <main>
-        <h1> Welcome to the home page of our Website, located in our Website Folder!</h1> 
-    <h2>Once we start placing our content in our containers, we can remove the height!</h2>
-        <p>A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea.A paragraph consists of one or more sentences. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.</P>
-
-        </main>
+<main>
+<h2> About me </h2>
+<img class="img-jay1"src="images/jay1.jpg" alt="my jay1">
+<p>Hello, my name is Ja'Mari Adams. I was born and raised in Seattle Washington 
+    and I graduated from Chief Sealth High School in 2010. 
+    I enjoy spending time with friends, playing videos games and trying new foods.</p>
+<p> <a href="website/about me website/aboutme.html">Click this link to view my "about me page"</a> </p>
+<p> <a href="website/about me website/resume.html">Click this link to view my "Resume page"</a> </p>
+    <h3>MAMP Local Host- Screen shot:   </h3>
+   <p> <img class="img-localhost" src="images/localhost.PNG" alt="" width="400" height="200"> </p>
+ 
+    <h4> MAMP Error- Screen shot:  </h4>
+    <p> <img class="img-localhost-error" src="images/localhost-error.png" alt="" width="400" height="200"> </p>
+</main>
 
 <aside>
-  <h3> This is my wonderful aside container! </h3>
+    <h1>Class Exercises </h1>
 
-</aside>
+    <h2>Week 1</h2>
+    <ul>
+        <li><a href="website/index.php">index.php</a></li>
+        
+    </ul>
+    <h2>Week 2</h2>
+    <ul>
+        <li><a href="weeks/week2/var.php">Var.php</a></li>
+        <li><a href="weeks/week2/currency-logic1.php">Currency-logic.php</a></li>
+        <li><a href="weeks/week2/currency1.php">Currency.php</a></li>
+        
+    </ul>
+    <h2>Week 3</h2>
+    <ul>
+        <li><a href="weeks/week3/arrays.php">arrays.php</a></li>
+        <li><a href="weeks/week3/if-else.php">if-else.php</a></li>
+        <li><a href="weeks/week3/heredoc.php">heredoc.php</a></li>
+        <li><a href="weeks/week3/date.php">date.php</a></li>
+        <li><a href="weeks/week3/for-loop.php">for-loop.php</a></li>
+        <li><a href="weeks/week3/switch.php">switch.php</a></li>
+        
+    </ul>
+    
+    <h2>Week 4</h2>
+    <ul>
+        <li><a href="weeks/week4/adder.php">adder.php</a></li>
+        <li><a href="weeks/week4/arithmetic-form.php">arithmetic-form.php</a></li>
+        <li><a href="weeks/week4/celcius.php">celcius.php</a></li>
+        <li><a href="weeks/week4/form1.php">form1.php</a></li>
+        <li><a href="weeks/week4/form2.php">form2.php</a></li>
+        <li><a href="weeks/week4/form3.php">form3.php</a></li>
+        <li><a href="weeks/week4/form-get.php">form-get.php</a></li>
+    </ul>
 
-</div> <!--end wrapper--> 
-<?php
-include ('includes/footer.php')
-?>
+    <h2>Week 5</h2>
+    <ul>
+        <li><a href="weeks/week5/currency.php">currency.php</a></li>
+        <li><a href="weeks/week5/currency2.php">currency2.php</a></li>
+        <li><a href="weeks/week5/CurrencyCalculator.php">CurrencyCalculator.php</a></li>
+        <li><a href="weeks/week5/null.php">null.php</a></li>
+    </ul>
+    <h2>Week 6</h2>
+    <ul>
+        <li><a href="weeks/week6/form.php">form.php</a></li>
+        <li><a href="weeks/week6/form2.php">form2.php</a></li>
+        <li><a href="weeks/week6/functions.php">functions.php</a></li>
+        
+    </ul>
+    <h2>Week 7</h2>
+    <ul>
+        <li><a href="weeks/week7/form3.php">form3.php</a></li>
+        <li><a href="weeks/week7/pictures.php">pictures.php</a></li>
+        <li><a href="weeks/week7/random.php">random.php</a></li>
+        <li><a href="weeks/week7/strings.php">strings.php</a></li>
+    </ul>
+
+    </ul>
+    <h2>Week 8</h2>
+    <ul>
+        <li><a href="weeks/week8/config.php">config.php</a></li>
+        <li><a href="weeks/week8/credentials.php">credentials.php</a></li>
+        <li><a href="weeks/week8/footer.php">footer.php</a></li>
+        <li><a href="weeks/week8/header.php">header.php</a></li>
+        <li><a href="weeks/week8/people.php">people.php</a></li>
+        <li><a href="weeks/week8/people-view.php">people-view.php</a></li>
+    </ul>
+</aside> 
+
+        </div> <!--end wrapper-->
+<footer>
+    <div id="inner-footer">
+    <ul>
+        <li>Copyright &copy;</li>
+
+        <li>All Rights Reserved</li>
+
+        <li><a href="../index.php">Web Design by jamari</a></li>
+
+        <li><a href="">HTML Validation</a></li>
+
+        <li><a href="">CSS Validation</a></li>
+
+    </ul>
+</div> <!--end inner footer-->
+</footer> 
+
+    </body>
+    </html>
